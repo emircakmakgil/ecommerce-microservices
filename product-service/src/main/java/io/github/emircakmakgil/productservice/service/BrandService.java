8f05205db5c06a66402786aef4d0807c5f8515f6
emircakmakgil <emircakmakgil@marun.edu.tr>
2025-09-13 14:00:57 +0300

    add product service dto  features

--- product-service/src/main/java/io/github/emircakmakgil/productservice/service/BrandService.java
+++ product-service/src/main/java/io/github/emircakmakgil/productservice/service/BrandService.java
@@ -1,4 +1,19 @@
 package io.github.emircakmakgil.productservice.service;
 
+import io.github.emircakmakgil.productservice.dto.BrandDto.BrandListiningDto;
+import io.github.emircakmakgil.productservice.dto.BrandDto.CreateBrandDto;
+import io.github.emircakmakgil.productservice.dto.BrandDto.DeleteBrandDto;
+import io.github.emircakmakgil.productservice.dto.BrandDto.UpdateBrandDto;
+import io.github.emircakmakgil.productservice.entity.Brand;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface BrandService {
+    List<Brand> findAll(List<UUID> brand);
+    Brand findById(UUID id);
+    void add(CreateBrandDto createBrandDto);
+    List<BrandListiningDto> getAll();
+    Brand update(UpdateBrandDto updateBrandDto);
+    void delete(DeleteBrandDto deleteBrandDto);
 }
